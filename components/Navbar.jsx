e624e2351b0ac5cffd1b2890c434fde6de5b3673
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-05-12 21:35:26 -0500

    17. Custom ActiveLink

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,12 +1,12 @@
-import Link from 'next/link';
+import { ActiveLink } from './ActiveLink';
 import styles from './Navbar.module.css';
 
 export const Navbar = () => {
 	return (
 		<nav className={styles['menu-container']}>
-			<Link href="/">Home</Link>
-			<Link href="/about">About</Link>
-			<Link href="/contact">Contact</Link>
+			<ActiveLink text="Home" href="/" />
+			<ActiveLink text="About" href="/about" />
+			<ActiveLink text="Contact" href="/contact" />
 		</nav>
 	);
 };
