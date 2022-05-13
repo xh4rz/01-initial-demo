2f0fa1ffb41eb202ec2ad918865ad981ae81d843
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-05-13 00:42:18 -0500

    20. Multiples Layouts anidados

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,9 +1,10 @@
 import Link from 'next/link';
+import { DarkLayout } from '../components/layouts/DarkLayout';
 import { MainLayout } from '../components/layouts/MainLayout';
 
 export default function AboutPage() {
 	return (
-		<MainLayout>
+		<>
 			<h1>About Page</h1>
 			<h1 className={'title'}>
 				Ir a <Link href="/">Home</Link>
@@ -11,6 +12,14 @@ export default function AboutPage() {
 			<p className={'description'}>
 				Get started by editing <code className={'code'}>pages/about.jsx</code>
 			</p>
-		</MainLayout>
+		</>
 	);
 }
+
+AboutPage.getLayout = function getLayout(page) {
+	return (
+		<MainLayout>
+			<DarkLayout>{page}</DarkLayout>
+		</MainLayout>
+	);
+};
