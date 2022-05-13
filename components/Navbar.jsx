2729e5536cfaa5e6fc4f3f720679cd8910e25490
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-05-13 01:00:49 -0500

    21. Tarea - Resumen de la sección

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,12 +1,31 @@
 import { ActiveLink } from './ActiveLink';
 import styles from './Navbar.module.css';
 
+const menuItems = [
+	{
+		text: 'Home',
+		href: '/'
+	},
+	{
+		text: 'About',
+		href: '/about'
+	},
+	{
+		text: 'Contact',
+		href: '/contact'
+	},
+	{
+		text: 'Pricing',
+		href: '/pricing'
+	}
+];
+
 export const Navbar = () => {
 	return (
 		<nav className={styles['menu-container']}>
-			<ActiveLink text="Home" href="/" />
-			<ActiveLink text="About" href="/about" />
-			<ActiveLink text="Contact" href="/contact" />
+			{menuItems.map(({ text, href }) => (
+				<ActiveLink key={href} text={text} href={href} />
+			))}
 		</nav>
 	);
 };
--- pages/pricing/index.jsx
+++ pages/pricing/index.jsx
@@ -1,16 +1,16 @@
 import Link from 'next/link';
 import { MainLayout } from '../../components/layouts/MainLayout';
 
-export default function ContactPage() {
+export default function pricingPage() {
 	return (
 		<MainLayout>
-			<h1>Contact Page</h1>
+			<h1>Pricing Page</h1>
 			<h1 className={'title'}>
 				Ir a <Link href="/">Home</Link>
 			</h1>
 			<p className={'description'}>
 				Get started by editing{' '}
-				<code className={'code'}>pages/contact/index.jsx</code>
+				<code className={'code'}>pages/pricing/index.jsx</code>
 			</p>
 		</MainLayout>
 	);
